10ab4b3e3a330f595f3eba4d47f84826e948da92
Seth Hobson <sethlawrencehobson@gmail.com>
2024-10-16 23:00:19 -0400

    added useEffect for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
+
+import RecipesApi from './api/api'
+
+import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [recipes, setRecipes] = useState({});
+
+  useEffect(function getRecipesOnMount() {
+    async function getRecipes() {
+      const recipesData = await RecipesApi.getRecipes();
+      setRecipes(recipesData);
+      setRecipes(recipes);
+      setIsLoading(false);
+    }
+    getRecipes();
+  })
+
 
   return (
     <>
-
+    {isLoading ? <div>Loading...</div> : recipes}
     </>
   )
 }
-
-
